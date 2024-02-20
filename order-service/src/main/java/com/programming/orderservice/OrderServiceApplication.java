7c2a1fd4fd057f272e95dfef2d6e57c0ccbc56b9
ManishJoshi <joshi8763@gmail.com>
2024-02-21 00:04:57 +0530

    dependency correction

--- order-service/src/main/java/com/programming/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/programming/orderservice/OrderServiceApplication.java
@@ -1,13 +1,13 @@
 package com.programming.orderservice;
 
-import com.programming.orderservice.config.WebClientConfig;
+//import com.programming.orderservice.config.WebClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(WebClientConfig.class)
+//@Import(WebClientConfig.class)
 @ComponentScan(basePackages = "com.programmingtechie.orderservice")
 public class OrderServiceApplication {
 	public static void main(String[] args) {
--- product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
@@ -1,8 +1,8 @@
 package com.programming.productservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.programmingtechie.productservice.dto.ProductRequest;
-import com.programmingtechie.productservice.repository.ProductRepository;
+import com.programming.productservice.dto.ProductRequest;
+import com.programming.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
